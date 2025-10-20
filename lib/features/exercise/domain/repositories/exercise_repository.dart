844c27b099254c2fa2a6a0bdee6ca9de59a55405
WorfZeBard <jexis34worf@gmail.com>
2025-10-20 09:31:13 +0800

    Add exercises feature

---
+++ lib/features/exercise/domain/repositories/exercise_repository.dart
@@ -0,0 +1,10 @@
+// lib/features/exercise/domain/repositories/exercise_repository.dart
+import 'package:dartz/dartz.dart';
+import '../entities/exercise_entity.dart';
+import '../../../user/domain/failures/failure.dart';
+
+abstract class ExerciseRepository {
+  Future<Either<Failure, List<ExerciseEntity>>> getExercises();
+  Future<Either<Failure, void>> addExercise(ExerciseEntity exercise);
+  Future<Either<Failure, void>> deleteExercise(String exerciseId);
+}
---
+++ lib/features/exercise/domain/usecases/get_exercises.dart
@@ -0,0 +1,14 @@
+// get_exercises.dart
+import 'package:dartz/dartz.dart';
+import '../entities/exercise_entity.dart';
+import '../repositories/exercise_repository.dart';
+import '../../../user/domain/failures/failure.dart';
+
+class GetExercises {
+  final ExerciseRepository repository;
+  GetExercises(this.repository);
+
+  Future<Either<Failure, List<ExerciseEntity>>> call() async {
+    return await repository.getExercises();
+  }
+}
